0404ff9758f30fbdb974b2f144178f96b4da5ded
Peter <peter@moodle.com>
2018-09-25 10:44:04 +0800

    Delete plugin folder

--- admin/tool/peterRd/README.md
+++
@@ -1,1 +0,0 @@
-New plugin creation test
--- admin/tool/peterRd/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'My first Moodle plugin';
-$plugin->version = 2018092500; // Plugin released on 21th September 2019.
-$plugin->release = '1.0';
