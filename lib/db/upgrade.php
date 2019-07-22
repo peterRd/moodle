b326ef12d0fc2ce2517a6065ed3e8b1ce101e166
Peter <peter@moodle.com>
2019-07-22 11:18:14 +0800

    MDL-66072 core: Remove course sharing and related capabilities

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -3090,5 +3090,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2018120305.04);
     }
 
+    if ($oldversion < 2018120305.09) {
+        $capabilitiestoberemoved = ['moodle/restore:restoretargethub', 'moodle/course:publish', 'moodle/backup:backuptargethub'];
+
+        // Delete any role_capabilities for the old roles.
+        $DB->delete_records_list('role_capabilities', 'capability', $capabilitiestoberemoved);
+
+        // Delete the capability itself.
+        $DB->delete_records_list('capabilities', 'name', $capabilitiestoberemoved);
+
+        // Remove unused config.
+        unset_config('enablecoursepublishing');
+        upgrade_main_savepoint(true, 2018120305.09);
+    }
+
     return true;
 }
