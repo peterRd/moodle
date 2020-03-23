d40c088fb229fcc4456366ab79f0b846c20e7906
Adrian Greeve <abgreeve@gmail.com>
2020-03-23 16:09:39 +0800

    MDL-67885 lib: Add moodlenet user profile information.

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2212,5 +2212,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2020013000.01);
     }
 
+    if ($oldversion < 2020032000.01) {
+
+        // Define field moodlenetprofile to be added to user.
+        $table = new xmldb_table('user');
+        $field = new xmldb_field('moodlenetprofile', XMLDB_TYPE_CHAR, '255', null, null, null, null, 'alternatename');
+
+        // Conditionally launch add field moodlenetprofile.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2020032000.01);
+    }
+
     return true;
 }
