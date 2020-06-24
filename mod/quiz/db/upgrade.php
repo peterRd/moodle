35b15a92687a2c580e0c102e0121bd53a31fb026
Peter Dias <peter@moodle.com>
2020-06-24 11:55:01 +0800

    MDL-52206 quiz: Remove existing completionpass grading in quiz

--- mod/quiz/db/upgrade.php
+++ mod/quiz/db/upgrade.php
@@ -60,5 +60,36 @@ function xmldb_quiz_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2020061501, 'quiz');
     }
 
+    if ($oldversion < 2021052501) {
+        $table = new xmldb_table('quiz');
+        $field = new xmldb_field('completionpass');
+
+        if ($dbman->field_exists($table, $field)) {
+            $sql = "SELECT q.id id " .
+                     "FROM {quiz} q " .
+               "INNER JOIN {course_modules} cm ON cm.instance = q.id " .
+               "INNER JOIN {modules} m ON m.id = cm.module " .
+                    "WHERE m.name = :name AND q.completionpass = :completionpass";
+
+            do {
+                // Get all quiz records with active completion grade pass set.
+                if ($records = $DB->get_records_sql($sql, ['name' => 'quiz', 'completionpass' => 1], 0, 1000)) {
+                    list($insql, $params) = $DB->get_in_or_equal($records, SQL_PARAMS_NAMED);
+                    $sql = "UPDATE {course_modules} cm " .
+                              "SET cm.completionpassgrade = 1 " .
+                            "WHERE cm.instance $insql";
+                    $DB->execute($sql, $params);
+                }
+            } while ($records);
+
+            // We have completed our checks. Drop the field.
+            if ($dbman->field_exists($table, $field)) {
+                $dbman->drop_field($table, $field);
+            }
+        }
+
+        upgrade_mod_savepoint(true, 2021052501, 'quiz');
+    }
+
     return true;
 }
