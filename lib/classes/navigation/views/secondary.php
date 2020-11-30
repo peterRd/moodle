bda2ae9714ac218edf809fd8f3686a5c1681ac66
Peter Dias <peter@moodle.com>
2020-11-30 13:47:18 +0800

    MDL-70200 core: Bring across the settingsnav into the secondary

--- lib/classes/navigation/views/secondary.php
+++ lib/classes/navigation/views/secondary.php
@@ -26,7 +26,11 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace core_navigation\views;
+namespace core\navigation\views;
+
+use navigation_node;
+use navigation_cache;
+use navigation_node_collection;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -34,28 +38,249 @@ defined('MOODLE_INTERNAL') || die();
  * Class secondary_navigation_view.
  *
  * The secondary navigation view is a stripped down tweaked version of the
- * settings_navigation
+ * settings_navigation/navigation
  *
  * @package     core
  * @category    navigation
  * @copyright   2020 onwards Peter Dias
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class secondary_navigation_view extends \navigation_node {
+class secondary extends navigation_node {
+    /** @var stdClass the current context */
+    protected $context;
+    /** @var moodle_page the moodle page that the navigation belongs to */
+    protected $page;
+    /** @var bool A switch to see if the navigation node is initialised */
+    protected $initialised = false;
+    /** @var navigation_cache **/
+    protected $cache;
 
     /**
      * secondary_navigation_view constructor.
      * @param \moodle_page $page
      */
     public function __construct(\moodle_page &$page) {
-        // TODO: Modify params on a as needed basis
-        // TODO: Do we need to initialise settingsnavigation here before proceeding?
+        global $FULLME;
+
+        if (during_initial_install()) {
+            return false;
+        }
+
+        $this->page = $page;
+        $this->context = $this->page->context;
+
+        // Not all pages override the active url. Do it now.
+        if ($this->page->has_set_url()) {
+            self::override_active_url(new \moodle_url($this->page->url));
+        } else {
+            self::override_active_url(new \moodle_url($FULLME));
+        }
+
+        // Initialise the navigation cache.
+        $this->cache = new navigation_cache(NAVIGATION_CACHE_NAME);
+        $this->children = new navigation_node_collection();
     }
 
     /**
      * Initialise the view based navigation based on the current context.
+     *
+     * As part of the initial restructure, the secondary nav is only considered for the following pages:
+     * 1 - Site admin settings
+     * 2 - Course page - Does not include front_page which has the same context.
+     * 3 - Module page
      */
     public function initialise() {
+        global $SITE;
 
+        if (during_initial_install()) {
+            return false;
+        } else if ($this->initialised) {
+            return true;
+        }
+        $this->id = 'secondary_navigation';
+        $context = $this->context;
+
+        switch ($context->contextlevel) {
+            case CONTEXT_COURSE:
+                if ($this->page->course->id != $SITE->id) {
+                    $this->load_course_navigation();
+                }
+                break;
+            case CONTEXT_MODULE:
+                $this->load_module_navigation();
+                break;
+            case CONTEXT_SYSTEM:
+                $this->load_admin_navigation();
+                break;
+        }
+
+        // Search and set the active node.
+        $this->search_for_active_node();
+        $this->initialised = true;
+    }
+
+    /**
+     * Load the course secondary navigation. Since we are sourcing all the info from existing objects that already do
+     * the relevant checks, we don't do it again here.
+     *
+     * @throws \coding_exception
+     * @throws \moodle_exception
+     */
+    protected function load_course_navigation() {
+        $course = $this->page->course;
+
+        // Initialise the main navigation and settings nav.
+        // It is most important that this is done before we try anything.
+        $settingsnav = $this->page->settingsnav;
+        $navigation = $this->page->navigation;
+
+        $url = new \moodle_url('/course/view.php', ['id' => $course->id, 'sesskey' => sesskey()]);
+        $this->add(get_string('coursepage', 'admin'), $url, self::TYPE_COURSE, null, 'coursehome');
+
+        $nodes['settings'] = [
+            self::TYPE_CONTAINER => [
+                'coursereports' => 3,
+                'questionbank' => 4
+            ],
+            self::TYPE_SETTING => [
+                'editsettings' => 0,
+                'coursecompletion' => 6,
+                'gradebooksetup' => 2.1,
+                'outcomes' => 2.2
+            ]
+        ];
+        $nodes['navigation'] = [
+            self::TYPE_CONTAINER => [
+                'participants' => 1,
+            ],
+            self::TYPE_SETTING => [
+                'badgesview' => 7,
+                'competencies' => 8,
+                'grades' => 2,
+            ],
+            self::TYPE_CUSTOM => [
+                'contentbank' => 5
+            ]
+        ];
+
+        $nodesordered = $this->get_leaf_nodes($settingsnav, $nodes['settings']);
+        $nodesordered += $this->get_leaf_nodes($navigation, $nodes['navigation']);
+        ksort($nodesordered);
+
+        foreach ($nodesordered as $key => $node) {
+            // If the key is a string then we are assuming this is a nested element.
+            if (is_string($key)) {
+                $parentnode = $nodesordered[floor($key)] ?? null;
+                if ($parentnode) {
+                    $parentnode->add_node($node);
+                }
+            } else {
+                $this->add_node($node);
+            }
+        }
+
+        // All additional nodes will be available under the 'Course admin' page.
+        $text = get_string('courseadministration');
+        $url = new \moodle_url('/course/admin.php', array('courseid' => $this->page->course->id));
+        $this->add($text, $url, null, null, 'courseadmin', new \pix_icon('t/edit', $text));
+    }
+
+    /**
+     * Get the leaf nodes for the nav view
+     *
+     * @param object $source The settingsnav OR navigation object
+     * @param array $nodes An array of nodes to fetch from the source which specifies the node type and final order
+     * @return array $nodesordered The fetched nodes ordered based on final specification.
+     */
+    protected function get_leaf_nodes($source, $nodes) {
+        $nodesordered = [];
+        foreach ($nodes as $type => $leaves) {
+            foreach ($leaves as $leaf => $location) {
+                if ($node = $source->find($leaf, $type)) {
+                    $nodesordered["$location"] = $node;
+                }
+            }
+        }
+
+        return $nodesordered;
+    }
+
+    /**
+     * Get the module's secondary navigation. This is based on settings_nav and would include plugin nodes added via
+     * '_extend_settings_navigation'.
+     * It populates the tree based on the nav mockup
+     *
+     * If nodes change, we will have to explicitly call the callback again.
+     */
+    protected function load_module_navigation() {
+        $settingsnav = $this->page->settingsnav;
+        $mainnode = $settingsnav->find('modulesettings', self::TYPE_SETTING);
+        $nodes = [
+            self::TYPE_SETTING => [
+                'modedit' => 1,
+                'roleoverride' => 3,
+                'logreport' => 4,
+                'filtermanage' => 8,
+                'backup' => 9,
+                'rolecheck' => 3.1,
+                'restore' => 10,
+                'competencybreakdown' => 11,
+                "mod_{$this->page->activityname}_useroverrides" => 5, // Overrides are module specific.
+                "mod_{$this->page->activityname}_groupoverrides" => 6,
+                'roleassign' => 7,
+            ],
+            self::TYPE_CUSTOM => [
+                'advgrading' => 2,
+            ]
+        ];
+        // Nodes that require a custom construct.
+        $customstructure = ['rolecheck'];
+
+        if ($mainnode) {
+            $this->add(get_string('module', 'course'), $this->page->url);
+            // Add the initial nodes.
+            $nodesordered = $this->get_leaf_nodes($mainnode, $nodes);
+            ksort($nodesordered);
+            foreach ($nodesordered as $key => $node) {
+                // If the key is a string then we are assuming this is a nested element.
+                if (is_string($key)) {
+                    $parentnode = $nodesordered[floor($key)] ?? null;
+                    if ($parentnode) {
+                        $parentnode->add_node($node);
+                    }
+                } else {
+                    $this->add_node($node);
+                }
+            }
+
+            // We have finished inserting the initial structure.
+            // Populate the menu with the rest of the nodes available.
+            $populatedkeys = $this->get_children_key_list();
+            $existingkeys = $mainnode->get_children_key_list();
+            $leftover = array_diff($existingkeys, $populatedkeys);
+            foreach ($leftover as $key) {
+                if (!in_array($key, $customstructure) && $leftovernode = $mainnode->get($key)) {
+                    $this->add_node($leftovernode);
+                }
+            }
+        }
+    }
+
+    /**
+     * Load the site admin navigation
+     */
+    protected function load_admin_navigation() {
+        $settingsnav = $this->page->settingsnav;
+        $node = $settingsnav->find('root', self::TYPE_SITE_ADMIN);
+        if ($node) {
+            $siteadminnode = $this->add($node->text, "#link$node->key");
+            foreach ($node->children as $child) {
+                if ($child->display && !$child->is_short_branch()) {
+                    $this->add_node($child);
+                } else {
+                    $siteadminnode->add_node($child);
+                }
+            }
+        }
     }
 }
